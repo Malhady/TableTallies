42e44cbf1b3db4b2b20bd95ff762bc131c1c0ce2
cameronryde <33267555+cameronryde@users.noreply.github.com>
2021-03-10 22:01:26 -0500

    Static sidebar overlapping content

---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Sidebar.css'
+
+const Sidebar = () =>(
+    <div className="Sidebar">
+        <h1 className = "Sidebar_content">Filter</h1>
+        <h1 className = "Sidebar_content">Filter</h1>
+        <h1 className = "Sidebar_content">Filter</h1>
+        <h1 className = "Sidebar_content">Filter</h1>
+
+    </div>
+);
+
+export default Sidebar;
+
