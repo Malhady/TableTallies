d375fb4fd48535e4b588912ad96174da83623eac
rydecatwit <cameronryde@gmail.com>
2021-03-16 21:35:04 -0400

    WIP filtering buttons

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import './Sidebar.css'
+import {Button} from 'react-bootstrap';
+import './Sidebar.css';
+import {Filter} from '../../../server/index';
 
 const Sidebar = () =>(
     <div className="Sidebar">
+        <Button className = "Sidebar_content" variant="contained" color="primary" onClick={Filter(Pizza)}> 
+            Pizza 
+        </Button>
+        <Button className = "Sidebar_content" variant="contained" color="primary"> 
+            Smoothie
+        </Button>
         <h1 className = "Sidebar_content">Filter</h1>
         <h1 className = "Sidebar_content">Filter</h1>
         <h1 className = "Sidebar_content">Filter</h1>
