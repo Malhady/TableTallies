0b55eea84f17ea9402868c57e21f97646fc2c8e1
cameronryde <33267555+cameronryde@users.noreply.github.com>
2021-04-08 15:06:52 -0400

    changed all recipes to uniform size and added arrow to button

--- client/src/Components/SidebarButton.js
+++ client/src/Components/SidebarButton.js
@@ -5,17 +5,20 @@ import './SidebarButton.css';
 class SideBarButton extends Component {
   render() {
     var open = "close";
+    var text = "🡺";
 
     if (this.props.buttonOpen) {
       open = "open";
+      text = "🡸";
     } 
+
 
     return (
 
       <button id="roundButton"
               onMouseDown={this.props.handleMouseDown}
               className = {open}>
-              
+                {text}
       </button>
     );
   }
