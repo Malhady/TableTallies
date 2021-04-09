54466ce8283874487d0b13ea68fd293d8e61b021
cameronryde <33267555+cameronryde@users.noreply.github.com>
2021-04-09 12:49:42 -0400

    Initial commit with recipe resizing

--- client/src/Components/HomePage/Recipe.js
+++ client/src/Components/HomePage/Recipe.js
@@ -3,14 +3,14 @@ import style from '../recipe.module.css';
 const Recipe = ({title,calories,image,ingredients, recipeURL}) => {
     return(
         <div className={style.recipe}>
+        <a target = "_blank" href = {recipeURL}><img src ={image} alt={title.text}/></a>        
         <a target= "_blank" href = {recipeURL}><h1>{title}</h1></a>
+        <p>Calories: {Math.round(calories)}</p>
             <ol>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>Calories: {Math.round(calories)}</p>
-            <a target = "_blank" href = {recipeURL}><img src ={image} alt={title.text}/></a>        
         </div>
     );
 }
